9bed2a102394d16d88eca425dff4fa46a00cdbfb
melvikaf <melvikafa@gmail.com>
2025-03-15 10:33:15 -0700

    something

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-gitbar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
