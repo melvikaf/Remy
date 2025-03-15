058f8af646dffec0bb37f5f934c3c4b65ccf4f0b
britneybudiman <britney.budiman@gmail.com>
2025-03-15 14:49:10 -0700

    fixed user fetch yay - brit and mel

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
 import MusicGenerator from './src/components/MusicGenerator'; // Import the MusicGenerator component
+import UserFetch from './src/firebase_components/userFetch.js'; // Make sure the path is correct
 
 export default function App() {
+  // Debugging: Add a useEffect to log when the component is mounted
+  useEffect(() => {
+    console.log('App component mounted');
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
+      <Text>App Component Loaded</Text>  {/* This will show if the App component loads */}
       <MusicGenerator />
+      <UserFetch />  {/* Ensure this component is rendered */}
     </SafeAreaView>
   );
 }
